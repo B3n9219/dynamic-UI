57cea36e42f24c5d2106af9ccf3ae7b86e3e3f3a
B3n9219 <benkirk1441@gmail.com>
2025-07-24 11:45:29 +0100

    put dropdown functions in a DropDownManager class

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import "./styles.css";
 
-function showDropdown(event) {
-    const dropdown = event.target.closest(".dropdown")
-    const dropdownList = dropdown.querySelector(".dropdown-list")
-    dropdownList.classList.add("visible")
-}
+export class DropdownManager {
+    show(event) {
+        const dropdown = event.target.closest(".dropdown")
+        const dropdownList = dropdown.querySelector(".dropdown-list")
+        dropdownList.classList.add("visible")
+    }
 
-function hideDropdown(event) {
-    const dropdown = event.target
-    const dropdownList = dropdown.querySelector(".dropdown-list")
-    dropdownList.classList.remove("visible")
+    hide(event) {
+        const dropdown = event.target
+        const dropdownList = dropdown.querySelector(".dropdown-list")
+        dropdownList.classList.remove("visible")
+    }
 }
+const dropdownManager = new DropdownManager();
+
+
 
 const dropdownTitle = document.querySelector(".dropdown .title")
-dropdownTitle.addEventListener("mouseenter", showDropdown)
+dropdownTitle.addEventListener("mouseenter", dropdownManager.show)
 
 const dropdown = document.querySelector(".dropdown")
-dropdown.addEventListener("mouseleave", hideDropdown)
+dropdown.addEventListener("mouseleave", dropdownManager.hide)
