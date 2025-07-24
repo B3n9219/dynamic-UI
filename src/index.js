a574540e0ad0fa166686a6397cdb819b23abeb7f
B3n9219 <benkirk1441@gmail.com>
2025-07-24 11:31:55 +0100

    Created the dropdown

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./styles.css";
+
+function showDropdown(event) {
+    const dropdown = event.target.closest(".dropdown")
+    const dropdownList = dropdown.querySelector(".dropdown-list")
+    dropdownList.classList.add("visible")
+}
+
+function hideDropdown(event) {
+    const dropdown = event.target
+    const dropdownList = dropdown.querySelector(".dropdown-list")
+    dropdownList.classList.remove("visible")
+}
+
+const dropdownTitle = document.querySelector(".dropdown .title")
+dropdownTitle.addEventListener("mouseenter", showDropdown)
+
+const dropdown = document.querySelector(".dropdown")
+dropdown.addEventListener("mouseleave", hideDropdown)
